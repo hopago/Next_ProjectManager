d920197aecfd1cfb5534fd66f99bd929050639d2
hopago <132269386+hopago@users.noreply.github.com>
2023-12-21 20:41:59 +0900

    clerk authentication

---
+++ app/(platform)/(clerk)/layout.tsx
@@ -0,0 +1,6 @@
+
+export default function layout({ children }: { children: React.ReactNode }) {
+  return (
+    <main className="h-full flex items-center justify-center">{children}</main>
+  );
+}
---
+++ config/domain.ts
@@ -0,0 +1,4 @@
+export const domainConfig = {
+    name: "Taskify",
+    description: "협업, 프로젝트 관리, 생산성의 정점."
+}
